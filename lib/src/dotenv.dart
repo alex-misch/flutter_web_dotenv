a3a7cb41db9868d95b5361a022cbaf068697ac28
Aleksandr Mishchenko <h-alex@bk.ru>
2019-09-02 14:26:46 +0300

    fix(pubspec): support for flutter_web

--- lib/src/dotenv.dart
+++ lib/src/dotenv.dart
@@ -21,10 +21,10 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter/widgets.dart';
+import 'package:flutter_web/foundation.dart';
+import 'package:flutter_web/material.dart';
+import 'package:flutter_web/services.dart' show rootBundle;
+import 'package:flutter_web/widgets.dart';
 
 import './parser.dart';
 
